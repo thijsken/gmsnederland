92dd981aacbe1aa04d235ea9ea603031c0000ed8
thijsken <41241417+thijsken@users.noreply.github.com>
2025-05-09 12:01:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Tijdelijke opslag voor meldingen
 let meldingen = [];
